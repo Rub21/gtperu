c5a6116a5cb92b3ec17340c621030783292561bf
Rub21 <ruben@mapbox.com>
2016-01-24 10:37:55 -0500

    separando loas archivos a controller

---
+++ app/controllers/hotel.js
@@ -0,0 +1,47 @@
+var Hotel = require('./../models/hotel');
+module.exports = {
+  save: function(req, res, upload) {
+    upload(req, res, function(err) {
+      if (err) {
+        return res.end("Error uploading file.");
+      }
+      var data = req.body;
+      var files = req.files;
+      var hotel = new Hotel();
+      hotel.idhotel = 'hhh';
+      hotel.clase = "Hotel";
+      hotel.estado = true;
+      hotel.categoria = data.categoria;
+      hotel.nombre=data.nombre;
+      hotel.descripcion = data.descripcion;
+      hotel.direccion = data.direccion;
+      hotel.telefono = data.telefono;
+      hotel.sitio_web = data.sitio_web;
+      hotel.correo_elec = data.correo_electronico;
+      hotel.tipo_precio_hab = data.precio_de_habitacion;
+      hotel.formas_pago = data.formas_de_pago;
+      hotel.latitud = parseFloat(data.lat);
+      hotel.longitud = parseFloat(data.lon);
+      hotel.imagenes = [];
+      for (var i = 0; i < files.length; i++) {
+        hotel.imagenes.push({
+          url: files[i].filename
+        });
+      }
+      hotel.save(function(err) {
+        if (err)
+          res.send(err);
+        res.json({
+          message: 'Hotel created!'
+        });
+      });
+    });
+  },
+  findAll: function(req, res) {
+    Hotel.find(function(err, hoteles) {
+      if (err)
+        res.send(err);
+      res.json(hoteles);
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var Hotel = require('./app/models/hotel');
+var controllerHotel = require('./app/controllers/hotel');
 var path = require('path');
 var multer = require('multer');
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/geotur2');
+mongoose.connect('mongodb://localhost/geotura');
 
 var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({
@@ -43,49 +44,10 @@ router.get('/', function(req, res) {
 
 router.route('/hoteles')
   .post(function(req, res) {
-    upload(req, res, function(err) {
-      if (err) {
-        return res.end("Error uploading file.");
-      }
-      var data = req.body;
-      var files = req.files;
-      var hotel = new Hotel();
-      hotel.idhotel = 'hhh';
-      hotel.clase = "Hotel";
-      hotel.estado = true;
-      hotel.categoria = data.categoria;
-      hotel.descripcion = data.nombre;
-      hotel.direccion = data.direccion;
-      hotel.telefono = data.telefono;
-      hotel.sitio_web = data.sitio_web;
-      hotel.correo_elec = data.correo_electronico;
-      hotel.tipo_precio_hab = data.precio_de_habitacion;
-      hotel.formas_pago = data.formas_de_pago;
-      hotel.latitud = parseFloat(data.lat);
-      hotel.longitud = parseFloat(data.lon);
-      hotel.imagenes = [];
-      for (var i = 0; i < files.length; i++) {
-        hotel.imagenes.push({
-          url: files[i].filename
-        });
-      }
-      hotel.save(function(err) {
-        if (err)
-          res.send(err);
-        res.json({
-          message: 'Hotel created!'
-        });
-        //res.sendFile(path.join(__dirname, 'confirm.html'));
-      });
-    });
+    controllerHotel.save(req, res, upload);
   })
   .get(function(req, res) {
-    Hotel.find(function(err, hoteles) {
-      if (err)
-        res.send(err);
-
-      res.json(hoteles);
-    });
+    controllerHotel.findAll(req, res);
   });
 
 app.use('/api', router);
