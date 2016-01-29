8bf3fad9d402bdc3ed97e944d544cebc0ae3f384
Rub21 <ruben@mapbox.com>
2016-01-28 22:07:17 -0500

    update

---
+++ install.sh
@@ -0,0 +1,34 @@
+    1  sudo apt-get install git
+    2  sudo apt-get -y update
+    3  sudo apt-get install -y nodejs npm
+    4  sudo ln -s /usr/bin/nodejs /usr/bin/node
+    5  git clone -b gh-pages https://github.com/Rub21/gtur.git
+    6  cd gtur/
+    7  ls
+    8  npm install
+    9  ls
+   10  npm start
+   11  npm install multer
+   12  npm start
+   13  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+   14  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse"
+   15  sudo apt-get install -y mongodb-org
+   16  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+   17  sudo apt-get update
+   18  sudo apt-get install -y mongodb-org
+   19  npm start
+   20  ls
+   21  cd public/
+   22  ls
+   23  cd admin/
+   24  ls
+   25  git pull
+   26  ls
+   27  git pull
+   28  npm start
+   29  cat p
+   30  cd ..
+   31  cd gtur/
+   32  git pull
+   33  npm start
+   34  history
